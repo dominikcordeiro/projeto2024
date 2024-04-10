67bb7f652fdd1dc3bf0b68ee5fae85ca8276aafe
dominik <114255612+dominikcordeiro@users.noreply.github.com>
2024-04-10 14:00:14 -0300

    10/04

--- dominik/js-projeto-aula-5/main.js
+++ dominik/js-projeto-aula-5/main.js
@@ -16,10 +16,13 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2023-02-01T00:00:00");
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
     function calculaTempo(tempoObjetivo) {
         let tempoAtual = new Date();
@@ -28,5 +31,19 @@ contadores[0].textContent = calculaTempo(tempoObjetivo1);
         let minutos = Math.floor(segundos / 60);
         let horas = Math.floor(minutos / 60);
         let dias = Math.floor(horas / 24);
+
+       segundos %= 60;
+       minutos %= 60;
+       horas %= 24;
+
         return dias + " dias " + " horas " + minutos + " minuto " + segundos + " segundo "
       }
+      function atualizarCronometro(){
+        for (let i=0; i<contadores.length;i++)
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    
+    }
+        
+    atualizarCronometro();
+    setInterval(atualizarCronometro,1000);
+    
